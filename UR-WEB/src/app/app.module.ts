9417466d43738d4b5d3dea974e36cb58f89618c9
Guruprasad <morasaguruprasad@gmail.com>
2019-08-17 21:57:49 +0530

    web project created agian

--- UR-WEB/src/app/app.module.ts
+++ UR-WEB/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountModule } from './account/account.module';
+import { AdminModule } from './admin/admin.module';
+import { ClientModule } from './client/client.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AccountModule,
+    AdminModule,
+    ClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
